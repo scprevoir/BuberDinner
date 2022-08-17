c04bb34ad42aad0c89f4ddfdaa1939f53c31cee0
Sérgio Carvalho <sergio.carvalho@prevoir.pt>
2022-08-17 23:36:22 +0100

    4v Global error handling

---
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    public class ErrorsController : ControllerBase
+    {
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            return Problem();
+        }
+    }
+}
---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            var problemDetails = new ProblemDetails
+            {
+                Type = "http://exemplo.com",
+                Title = "An error occured while processing your request.",
+                Status = (int)HttpStatusCode.InternalServerError,
+            };
+            var errorResult = new { error = "An error occured while processing your request." };
+
+            context.Result = new ObjectResult(problemDetails);
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
